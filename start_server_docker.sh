269ecffca6551a79b68cf24420497ac8c05ea994
Daniel Martin <xadlien@gmail.com>
2022-09-25 22:02:08 -0400

    added options for launch

--- start_server_docker.sh
+++ start_server_docker.sh
@@ -8,4 +8,4 @@ ServerAdminPassword="YourAdminPassword"
 maxplayers="50"
 
 # launch
-./ShooterGame/Binaries/Linux/ShooterGameServer 'TheIsland?listen?Multihome=0.0.0.0?SessionName=ServerName?MaxPlayers=70?QueryPort=27015?RCONPort=32330?Port=7777?ServerAdminPassword=password' -server -log
+./ShooterGame/Binaries/Linux/ShooterGameServer 'Ragnarok?listen?Multihome=0.0.0.0?SessionName=KnorrFamilyServer?MaxPlayers=70?QueryPort=27015?RCONPort=32330?Port=7777?ServerAdminPassword=adminpassword?AllowFlyerCarryPvE=true??AllowFlyingStaminaRecovery=true?ForceFlyerExplosives=true?PreventSpawnAnimations=true?AllowRaidDinoFeeding=true' -server -log -ForceAllowCaveFlyers -crossplay
